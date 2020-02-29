09ec8ba99477409a494176c66b1874f2b4ffb719
Your Name <you@example.com>
2020-02-29 23:37:25 +0200

    "Make A pipeline and confirm page and Confirm Mail"

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:personal_safety/Auth/signup.dart';
 import 'package:personal_safety/Auth/signupSuccessful.dart';
 import 'package:personal_safety/componants/card.dart';
 import 'package:get_it/get_it.dart';
+import 'package:personal_safety/services/service_confirm.dart';
 import 'package:personal_safety/services/service_login.dart';
 import 'package:personal_safety/services/service_register.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -17,6 +18,8 @@ import 'componants/test.dart';
 void setupLocator() {
   GetIt.instance.registerLazySingleton(() => LoginService());
   GetIt.instance.registerLazySingleton(() => RegisterService());
+  GetIt.instance.registerLazySingleton(() => ConfirmService());
+
 }
 
 Future<void> main() async {
@@ -26,7 +29,9 @@ Future<void> main() async {
   var token = prefs.getString('token');
   print(token);
   runApp(MaterialApp(
-      debugShowCheckedModeBanner: false, home: token == null ? Logout() : Test()
+      debugShowCheckedModeBanner: false,
+//      home: token == null ? Logout() : Test()
+  home: ConfirmEmail(),
 //  home: ConfirmCode(),
       ));
 }
--- lib/services/service_confirm.dart
+++ lib/services/service_confirm.dart
@@ -4,24 +4,34 @@ import 'package:personal_safety/models/api_response.dart';
 import 'package:personal_safety/models/confirm_mail.dart';
 
 class ConfirmService {
-  static var token = '';
+  static var result = '';
   static const API = 'https://personalsafety.azurewebsites.net/';
   static const headers = {'Content-Type': 'application/json'};
 
   // Logging In
   Future<APIResponse<dynamic>> confirmMail(ConfirmMailCredentials item) {
+    print('*****************');
+
     return http
-        .post(API + '/api/Account/ConfirmMail',
-            headers: headers, body: json.encode(item.toJson()))
+        .get(
+            API + '/api/Account/SendConfirmationMail' + '?email=' + item.email,
+            headers: headers)
         .then((data) {
+      print('??????????????????????');
       if (data.statusCode == 200) {
+
         Map userMap = jsonDecode(data.body);
         var APIresult = APIResponse.fromJson(userMap);
         print(APIresult.status);
         print(APIresult.result);
-        token = APIresult.result;
+        //result = APIresult.result;
         print(APIresult.hasErrors);
         return APIresult;
+      }
+      else{
+        print('============================');
+        print(data.statusCode);
+        print("-----------------------------");
       }
       return APIResponse<dynamic>(
           hasErrors: true, messages: "An Error Has Occured");

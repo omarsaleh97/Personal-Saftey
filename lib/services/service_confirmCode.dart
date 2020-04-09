6ca64b4f0a04acfb522483d58e77a2f8d8439094
omarelleboudy <omarelleboudy@gmail.com>
2020-04-09 23:34:50 +0200

    API changed

--- lib/services/service_confirmCode.dart
+++ lib/services/service_confirmCode.dart
@@ -3,16 +3,17 @@ import 'dart:convert';
 import 'package:personal_safety/models/api_response.dart';
 import 'package:personal_safety/models/confirm_mail.dart';
 import 'package:personal_safety/models/confirm_token.dart';
+import 'package:personal_safety/others/StaticVariables.dart';
 
 class ConfirmTokenService {
   static var token = '';
-  static const API = 'https://personalsafety.azurewebsites.net/';
+//  static const API = 'https://personalsafety.azurewebsites.net/';
   static const headers = {'Content-Type': 'application/json'};
 
   // Logging In
   Future<APIResponse<dynamic>> confirmToken(ConfirmTokenCredentials item) {
     return http
-        .post(API + '/api/Account/ConfirmMail',
+        .post(StaticVariables.API + '/api/Account/ConfirmMail',
         headers: headers, body: json.encode(item.toJson()))
         .then((data) {
       if (data.statusCode == 200) {
--- lib/services/service_forgetpassword.dart
+++ lib/services/service_forgetpassword.dart
@@ -1,11 +1,12 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:personal_safety/models/api_response.dart';
+import 'package:personal_safety/others/StaticVariables.dart';
 import 'package:personal_safety/models/forget_password.dart';
 
 class ForgetPasswordService {
   static var result = '';
-  static const API = 'https://personalsafety.azurewebsites.net/';
+//  static const API = 'https://personalsafety.azurewebsites.net/';
   static const headers = {'Content-Type': 'application/json'};
 
   // Logging In
@@ -13,7 +14,7 @@ class ForgetPasswordService {
     print('*****************');
 
     return http
-        .get(API + '/api/Account/ForgotPassword' + '?mail=' + item.email,
+        .get(StaticVariables.API + '/api/Account/ForgotPassword' + '?mail=' + item.email,
             headers: headers)
         .then((data) {
       print('??????????????????????');

2c2458ad3060561b2862778102c65c428c2be5f8
omarelleboudy <omarelleboudy@gmail.com>
2020-04-20 13:17:28 +0200

    Fully working Realtime SOS request - Merged with Karim Branch

--- lib/others/StaticVariables.dart
+++ lib/others/StaticVariables.dart
@@ -1,22 +1,18 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StaticVariables {
-
   //static const
 
-
-  static const
-                API = 'https://personal-safety.azurewebsites.net/', //Publish
-                //API = 'http://192.168.1.4:5566', //Test
-    clientServerURL = API + "/hubs/client",
-  realtimeServerURL = API + "/hubs/realtime";
+  static const API = 'https://personal-safety.azurewebsites.net/', //Publish
+      //API = 'http://192.168.1.4:5566', //Test
+      clientServerURL = API + "/hubs/client",
+      realtimeServerURL = API + "/hubs/realtime";
 
   static SharedPreferences prefs;
 
-static Future<void> Init() async
-{
-
-  prefs = await SharedPreferences.getInstance();
-
-}
+  static Future<void> Init() async {
+    prefs = await SharedPreferences.getInstance();
+    //static const API = 'http://192.168.1.4:5566'; //Test
+    const API = 'https://personal-safety.azurewebsites.net/'; //Publish
+  }
 }
